478c955200b043af6502510f2c91c521179ff259
Muhammad Tabish Rashid <muhammadtabish05@gmail.com>
2023-05-21 21:18:45 +0500

    Initial commit

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PerfectFit.Data;
+
+namespace PerfectFit.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ProductsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var ObjProductsList = _db.Product.ToList();
+            return View();
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PerfectFit.Models;
+
+namespace PerfectFit.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Products> Product { get; set; }
+
+    }
+}
